bb66b7c3c44e82b8d4cb298995967a90720b17a7
Andy Phan <AndyyyPhan@gmail.com>
2023-12-19 23:42:55 -0500

    Deleted unnecessary import statements (for now).

--- src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
+++ src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
@@ -1,14 +1,6 @@
 package com.andyphan.chessopeningtrainer;
 
-import jakarta.persistence.PersistenceException;
 import org.hibernate.*;
-import org.hibernate.query.Query;
-
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
 public class DatabaseManager {
