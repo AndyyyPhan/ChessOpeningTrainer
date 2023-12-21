ce35b138fa62b71d0fa1c1328d29b10e4ab5918b
Andy Phan <AndyyyPhan@gmail.com>
2023-12-20 23:35:33 -0500

    Finished Login scene to be able to login to main menu, create new user, and close application.

--- src/main/java/com/andyphan/chessopeningtrainer/Login.java
+++ src/main/java/com/andyphan/chessopeningtrainer/Login.java
@@ -1,11 +1,9 @@
 package com.andyphan.chessopeningtrainer;
 
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Login extends Scene {
     private DatabaseManager database;
@@ -26,5 +24,61 @@ public class Login extends Scene {
         Button closeButton = new Button("Close");
 
         loginLayout.getChildren().addAll(usernameLabel, usernameField, passwordLabel, passwordField, loginButton, createButton, closeButton);
+
+        loginButton.setOnAction(e -> {
+            // Check username and password
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            if (database.isValidPlayer(username, password)) {
+                // Successful login, transition to course search screen
+                Player player = new Player();
+                player.setUsername(username);
+                player.setPassword(password);
+                PlayerManager.setCurrentPlayer(player);
+                goToMainMenu(player);
+            } else {
+                showAlert("Invalid credentials! Please try again.");
+            }
+        });
+
+        // Set create button action
+        createButton.setOnAction(e -> {
+            String newUsername = usernameField.getText();
+            String newPassword = passwordField.getText();
+            if (newUsername.isEmpty() || newPassword.isEmpty()) {
+                showAlert("Please enter both username and password.");
+            } else if (database.isPlayerExisting(newUsername)) {
+                showAlert("Username already exists. Please enter correct password or create a different user.");
+            } else if (!database.validPasswordLength(newPassword)) {
+                showAlert("Please enter a password with at least 8 characters.");
+            } else {
+                database.addPlayer(newUsername, newPassword);
+                showSuccess("Successfully created account!");
+            }
+        });
+        closeButton.setOnAction(e -> {
+            Stage stage = (Stage) loginLayout.getScene().getWindow();
+            stage.close();
+        });
+    }
+
+    private void goToMainMenu(Player player) {
+        SceneManager.setScene(new MainMenu(player));
+    }
+
+    private void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Success");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
