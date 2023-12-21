6d46869da48e2a47c31086d36490b11c5f1d826b
Andy Phan <AndyyyPhan@gmail.com>
2023-12-20 23:34:38 -0500

    Added methods in DatabaseManager: isPlayerExisting, isValidPlayer, validPasswordLength, addPlayer

--- src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
+++ src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
@@ -1,5 +1,6 @@
 package com.andyphan.chessopeningtrainer;
 
+import jakarta.persistence.PersistenceException;
 import org.hibernate.*;
 import java.util.logging.Level;
 
@@ -13,5 +14,41 @@ public class DatabaseManager {
 
     public void close() {
         session.close();
+    }
+
+    public boolean isPlayerExisting(String username) {
+        Player existingPlayer = session.createQuery("FROM Player WHERE username = :username", Player.class)
+                .setParameter("username", username)
+                .uniqueResult();
+        return existingPlayer != null;
+    }
+
+    public boolean isValidPlayer(String username, String password) {
+        if (!isPlayerExisting(username)) return false;
+
+        Player existingPlayer = session.createQuery("FROM Player WHERE username = :username", Player.class)
+                .setParameter("username", username)
+                .uniqueResult();
+        return existingPlayer.getPassword().equals(password);
+    }
+
+    public boolean validPasswordLength(String password) {
+        return password.length() >= 8;
+    }
+
+    public void addPlayer(String username, String password) {
+        session.beginTransaction();
+        try {
+            Player player = new Player();
+            player.setUsername(username);
+            player.setPassword(password);
+            session.persist(player);
+            session.getTransaction().commit();
+        }
+        catch (PersistenceException e) {
+            if (session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+        }
     }
 }
