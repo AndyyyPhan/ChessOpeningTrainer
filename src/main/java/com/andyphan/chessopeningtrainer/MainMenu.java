1f066552c357f92dd0ce22912d0b14c6a26bf2aa
Andy Phan <AndyyyPhan@gmail.com>
2023-12-20 23:35:50 -0500

    Created MainMenu scene to see options after logging in.

---
+++ src/main/java/com/andyphan/chessopeningtrainer/MainMenu.java
@@ -0,0 +1,36 @@
+package com.andyphan.chessopeningtrainer;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+public class MainMenu extends Scene {
+    private DatabaseManager database;
+    public void initialize() {
+        database = new DatabaseManager();
+    }
+
+    public MainMenu(Player player) {
+        super(new VBox(10), 400, 300);
+        initialize();
+
+        Label mainMenuLabel = new Label("Main Menu");
+        mainMenuLabel.setFont(new Font(20));
+        Button practiceButton = new Button("Practice your openings!");
+        Button manageOpeningsButton = new Button("Manage your openings!");
+        Button logoutButton = new Button("Logout");
+        VBox loginLayout = (VBox) getRoot();
+        loginLayout.setAlignment(Pos.CENTER);
+        loginLayout.getChildren().addAll(mainMenuLabel, practiceButton, manageOpeningsButton, logoutButton);
+
+        logoutButton.setOnAction(e -> {
+            SceneManager.setScene(new Login());
+            PlayerManager.setCurrentPlayer(null);
+        });
+    }
+}
