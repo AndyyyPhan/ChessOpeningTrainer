e281fe23aff081930d710c0d72392e82249d8d12
Andy Phan <AndyyyPhan@gmail.com>
2023-12-27 22:31:04 -0500

    Added ChessOpening.java and added it as a mapping class in hibernate.cfg.xml

---
+++ src/main/java/com/andyphan/chessopeningtrainer/ChessOpening.java
@@ -0,0 +1,51 @@
+package com.andyphan.chessopeningtrainer;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ChessOpenings", uniqueConstraints = @UniqueConstraint(columnNames = {"fen", "moves", "name"}))
+public class ChessOpening {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "fen", nullable = false)
+    private String fen;
+    @Column(name = "moves", nullable = false)
+    private String moves;
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFen() {
+        return fen;
+    }
+
+    public void setFen(String fen) {
+        this.fen = fen;
+    }
+
+    public String getMoves() {
+        return moves;
+    }
+
+    public void setMoves(String moves) {
+        this.moves = moves;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
