f3bd42b0f24b51643138c1022f1730cccaaf970b
Andy Phan <AndyyyPhan@gmail.com>
2023-12-27 22:33:08 -0500

    Added ListView to hold current openings in practice

--- src/main/java/com/andyphan/chessopeningtrainer/ManageOpeningsMenu.java
+++ src/main/java/com/andyphan/chessopeningtrainer/ManageOpeningsMenu.java
@@ -1,8 +1,13 @@
 package com.andyphan.chessopeningtrainer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
@@ -17,6 +22,19 @@ public class ManageOpeningsMenu extends Scene {
         initialize();
         Label mainLabel = new Label("List of Openings in Practice");
         mainLabel.setFont(new Font(20));
-        mainLabel.setAlignment(Pos.CENTER);
+
+        HBox mainLabelContainer = new HBox();
+        mainLabelContainer.setAlignment(Pos.CENTER);
+        mainLabelContainer.getChildren().add(mainLabel);
+        HBox.setHgrow(mainLabel, Priority.ALWAYS);
+
+        ListView<ChessOpening> table = new ListView<>();
+        ObservableList<ChessOpening> chessOpenings = FXCollections.observableArrayList();
+        chessOpenings.addAll(database.getAllChessOpenings());
+
+        table.setItems(chessOpenings);
+
+        VBox layout = (VBox) getRoot();
+        layout.getChildren().addAll(mainLabelContainer, table);
     }
 }
