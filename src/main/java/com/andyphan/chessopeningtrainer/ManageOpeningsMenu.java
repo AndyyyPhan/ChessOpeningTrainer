bcda3043e3050ba061d2d217622826de99af5aff
Andy Phan <AndyyyPhan@gmail.com>
2023-12-30 16:11:29 -0500

    Added button to show all possible openings

--- src/main/java/com/andyphan/chessopeningtrainer/ManageOpeningsMenu.java
+++ src/main/java/com/andyphan/chessopeningtrainer/ManageOpeningsMenu.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
@@ -20,13 +21,19 @@ public class ManageOpeningsMenu extends Scene {
     public ManageOpeningsMenu(Player player) {
         super(new VBox(10), 800, 600);
         initialize();
+
         Label mainLabel = new Label("List of Openings in Practice");
         mainLabel.setFont(new Font(20));
-
         HBox mainLabelContainer = new HBox();
         mainLabelContainer.setAlignment(Pos.CENTER);
         mainLabelContainer.getChildren().add(mainLabel);
         HBox.setHgrow(mainLabel, Priority.ALWAYS);
+
+        Label allOpeningsLabel = new Label("Click here to show all openings: ");
+        allOpeningsLabel.setFont(new Font(15));
+        Button allOpeningsButton = new Button("All Openings");
+        allOpeningsButton.setFont(new Font(12));
+        HBox allOpeningsContainer = new HBox(5, allOpeningsLabel, allOpeningsButton);
 
         ListView<ChessOpening> table = new ListView<>();
         ObservableList<ChessOpening> chessOpenings = FXCollections.observableArrayList();
@@ -35,6 +42,6 @@ public class ManageOpeningsMenu extends Scene {
         table.setItems(chessOpenings);
 
         VBox layout = (VBox) getRoot();
-        layout.getChildren().addAll(mainLabelContainer, table);
+        layout.getChildren().addAll(mainLabelContainer, table, allOpeningsContainer);
     }
 }
