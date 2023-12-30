eb62c9c3191bda890ac5b6d84e075da2fbf9832a
Andy Phan <AndyyyPhan@gmail.com>
2023-12-30 16:14:29 -0500

    Added AllOpenings.java to store all possible openings in a list view. Will implement later.

---
+++ src/main/java/com/andyphan/chessopeningtrainer/AllOpenings.java
@@ -0,0 +1,10 @@
+package com.andyphan.chessopeningtrainer;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+public class AllOpenings extends Scene {
+    public AllOpenings() {
+        super(new VBox(10), 800, 600);
+    }
+}
