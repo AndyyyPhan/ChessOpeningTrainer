8497f1831924383c26a8c78ecbf276a260862135
Andy Phan <AndyyyPhan@gmail.com>
2024-01-04 15:50:14 -0500

    Replaced ListView with Pagination because dataset is too big. ListView causes error shown in console. Pagination splits the database into multiple pages.

--- src/main/java/com/andyphan/chessopeningtrainer/AllOpeningsMenu.java
+++ src/main/java/com/andyphan/chessopeningtrainer/AllOpeningsMenu.java
@@ -6,13 +6,19 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Pagination;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
+import java.io.IOException;
+import java.util.List;
+
 public class AllOpeningsMenu extends Scene {
-    public AllOpeningsMenu() {
+    private static int ITEMS_PER_PAGE = 300;
+    private List<String> allChessOpenings;
+    public AllOpeningsMenu() throws IOException {
         super(new VBox(10), 800, 600);
         Label mainLabel = new Label("List of All Openings");
         mainLabel.setFont(new Font(20));
@@ -21,12 +27,26 @@ public class AllOpeningsMenu extends Scene {
         mainLabelContainer.getChildren().add(mainLabel);
         HBox.setHgrow(mainLabel, Priority.ALWAYS);
 
-        ListView<ChessOpening> table = new ListView<>();
-        ObservableList<ChessOpening> allChessOpenings = FXCollections.observableArrayList();
+        ChessOpeningsParser chessOpeningsParser = new ChessOpeningsParser();
+        allChessOpenings = chessOpeningsParser.getAllChessOpeningNames();
 
-        table.setItems(allChessOpenings);
+        Pagination pagination = new Pagination(getPageCount(), 0);
+        pagination.setPageFactory(this::createPage);
+
 
         VBox layout = (VBox) getRoot();
-        layout.getChildren().addAll(mainLabelContainer, table);
+        layout.getChildren().addAll(mainLabelContainer, pagination);
+    }
+
+    private int getPageCount() {
+        return (int) Math.ceil((double) allChessOpenings.size() / ITEMS_PER_PAGE);
+    }
+
+    private ListView<String> createPage(int pageIndex) {
+        int fromIndex = pageIndex * ITEMS_PER_PAGE;
+        int toIndex = Math.min(fromIndex + ITEMS_PER_PAGE, allChessOpenings.size());
+        ObservableList<String> displayedChessOpenings = FXCollections.observableArrayList(
+                allChessOpenings.subList(fromIndex, toIndex));
+        return new ListView<>(displayedChessOpenings);
     }
 }
