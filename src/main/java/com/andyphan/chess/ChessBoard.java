d450c7af0a4813eb00e3e8a2d1a4b456aeab041f
Andy Phan <AndyyyPhan@gmail.com>
2024-01-06 23:26:20 -0500

    Changed private to protected so that other files can access the TILE_SIZE and BOARD_SIZE. Also, finished implementing initializePieces()

--- src/main/java/com/andyphan/chess/ChessBoard.java
+++ src/main/java/com/andyphan/chess/ChessBoard.java
@@ -1,15 +1,17 @@
 package com.andyphan.chess;
 
+import com.andyphan.chess.pieces.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class ChessBoard extends GridPane {
-    private static final int TILE_SIZE = 80;
-    private static final int BOARD_SIZE = 8;
+    protected static final int TILE_SIZE = 80;
+    protected static final int BOARD_SIZE = 8;
 
     public ChessBoard() {
         drawBoard();
+        initializePieces();
     }
 
     private void drawBoard() {
@@ -24,5 +26,32 @@ public class ChessBoard extends GridPane {
         Rectangle square = new Rectangle(TILE_SIZE, TILE_SIZE);
         square.setFill(color);
         add(square, col, row);
+    }
+
+    private void initializePieces() {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            add(new Pawn("whitePawn.png"), col, 6);
+            add(new Pawn("blackPawn.png"), col, 1);
+        }
+        add(new Knight("whiteKnight.png"), 1, 7);
+        add(new Knight("whiteKnight.png"), 6, 7);
+        add(new Knight("blackKnight.png"), 1, 0);
+        add(new Knight("blackKnight.png"), 6, 0);
+
+        add(new Bishop("whiteBishop.png"), 2, 7);
+        add(new Bishop("whiteBishop.png"), 5, 7);
+        add(new Bishop("blackBishop.png"), 2, 0);
+        add(new Bishop("blackBishop.png"), 5, 0);
+
+        add(new Rook("whiteRook.png"), 0, 7);
+        add(new Rook("whiteRook.png"), 7, 7);
+        add(new Rook("blackRook.png"), 0, 0);
+        add(new Rook("blackRook.png"), 7, 0);
+
+        add(new Queen("whiteQueen.png"), 3, 7);
+        add(new Queen("blackQueen.png"), 3, 0);
+
+        add(new King("whiteKing.png"), 4, 7);
+        add(new King("blackKing.png"), 4, 0);
     }
 }
