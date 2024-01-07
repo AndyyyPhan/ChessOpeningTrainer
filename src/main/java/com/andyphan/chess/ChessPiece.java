e9f3d488889c58e67fcc61c41968fb716aadbfab
Andy Phan <AndyyyPhan@gmail.com>
2024-01-07 18:03:31 -0500

    Deleted getResource()

--- src/main/java/com/andyphan/chess/ChessPiece.java
+++ src/main/java/com/andyphan/chess/ChessPiece.java
@@ -7,14 +7,9 @@ import java.util.Objects;
 
 public class ChessPiece extends ImageView {
     public ChessPiece(String imageName) {
-//        Image image = new Image(getResource("/pieces/" + imageName));
-        Image image = new Image(getClass().getResourceAsStream("/pieces/" + imageName));
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pieces/" + imageName)));
         setFitHeight(ChessBoard.TILE_SIZE);
         setFitWidth(ChessBoard.TILE_SIZE);
         setImage(image);
-    }
-
-    private static String getResource(String filename) {
-        return Objects.requireNonNull(ChessPiece.class.getResource(filename)).toExternalForm();
     }
 }
