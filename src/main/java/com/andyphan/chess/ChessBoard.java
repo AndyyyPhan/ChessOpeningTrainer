00694a7eeaca7b80b3c3061e68acb8727072d239
Andy Phan <AndyyyPhan@gmail.com>
2024-01-08 22:52:12 -0500

    Added columns and labels to the chessboard, so that users can see what each row and column represents

--- src/main/java/com/andyphan/chess/ChessBoard.java
+++ src/main/java/com/andyphan/chess/ChessBoard.java
@@ -1,8 +1,12 @@
 package com.andyphan.chess;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class ChessBoard extends GridPane {
     protected static final int TILE_SIZE = 80;
@@ -20,12 +24,48 @@ public class ChessBoard extends GridPane {
                     drawSquare(row, col, (row + col) % 2 == 0 ? Color.rgb(123, 124, 124) : Color.rgb(215, 215, 215));
                 }
             }
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                Text colLabel = new Text(String.valueOf((char) ('h' - col)));
+                colLabel.setFont(new Font(15));
+                colLabel.setStyle("-fx-font-weight: bold");
+                StackPane colPane = new StackPane(colLabel);
+                colPane.setMinSize(20, 20);
+                colPane.setAlignment(Pos.CENTER);
+                add(colPane, col, 8);
+            }
+            for (int row = 0; row < BOARD_SIZE; row++) {
+                Text rowLabel = new Text(String.valueOf(row+1));
+                rowLabel.setFont(new Font(15));
+                rowLabel.setStyle("-fx-font-weight: bold");
+                StackPane rowPane = new StackPane(rowLabel);
+                rowPane.setMinSize(20, 20);
+                rowPane.setAlignment(Pos.CENTER);
+                add(rowPane, 9, row);
+            }
         }
         else {
             for (int row = 0; row < BOARD_SIZE; row++) {
                 for (int col = 0; col < BOARD_SIZE; col++) {
                     drawSquare(row, col, (row + col) % 2 == 0 ? Color.rgb(215, 215, 215) : Color.rgb(123, 124, 124));
                 }
+            }
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                Text colLabel = new Text(String.valueOf((char) ('a' + col)));
+                colLabel.setFont(new Font(15));
+                colLabel.setStyle("-fx-font-weight: bold");
+                StackPane colPane = new StackPane(colLabel);
+                colPane.setMinSize(20, 20);
+                colPane.setAlignment(Pos.CENTER);
+                add(colPane, col, 8);
+            }
+            for (int row = 0; row < BOARD_SIZE; row++) {
+                Text rowLabel = new Text(String.valueOf(8 - row));
+                rowLabel.setFont(new Font(15));
+                rowLabel.setStyle("-fx-font-weight: bold");
+                StackPane rowPane = new StackPane(rowLabel);
+                rowPane.setMinSize(20, 20);
+                rowPane.setAlignment(Pos.CENTER);
+                add(rowPane, 9, row);
             }
         }
     }
