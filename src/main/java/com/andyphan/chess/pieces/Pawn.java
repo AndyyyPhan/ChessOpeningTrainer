6932ec83383e5604be28f63b37a75f0a84bda834
Andy Phan <AndyyyPhan@gmail.com>
2024-01-10 00:47:53 -0500

    Over-rid getCandidateMoves() for pawns. I still need to do kill. Maybe en-passant? I don't know if any chess openings use that. If not, then I won't implement.

--- src/main/java/com/andyphan/chess/pieces/Pawn.java
+++ src/main/java/com/andyphan/chess/pieces/Pawn.java
@@ -1,9 +1,25 @@
 package com.andyphan.chess.pieces;
 
+import com.andyphan.chess.Alliance;
 import com.andyphan.chess.ChessPiece;
+import com.andyphan.chess.PieceMoves;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Pawn extends ChessPiece {
+    private ArrayList<PieceMoves> candidateMoves = new ArrayList<>();
     public Pawn(String imagePath) {
         super(imagePath);
+    }
+
+    @Override
+    public ArrayList<PieceMoves> getCandidateMoves() {
+        candidateMoves.clear();
+        if ((getAlliance() == Alliance.WHITE && getRow() == 6) || (getAlliance() == Alliance.BLACK && getRow() == 1))
+            Collections.addAll(candidateMoves, PieceMoves.DOUBLE_MOVE, PieceMoves.MOVE);
+        else if ((getAlliance() == Alliance.WHITE && getRow() != 6) || (getAlliance() == Alliance.BLACK && getRow() != 1))
+            candidateMoves.add(PieceMoves.MOVE);
+        return candidateMoves;
     }
 }
