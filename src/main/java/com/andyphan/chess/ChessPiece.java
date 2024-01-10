eeef8afe2676afca29c983932834f8c308a8992f
Andy Phan <AndyyyPhan@gmail.com>
2024-01-09 23:35:24 -0500

    Implementing Alliance in ChessPiece class so that each chess piece has an alliance.

--- src/main/java/com/andyphan/chess/ChessPiece.java
+++ src/main/java/com/andyphan/chess/ChessPiece.java
@@ -6,10 +6,20 @@ import javafx.scene.image.ImageView;
 import java.util.Objects;
 
 public class ChessPiece extends ImageView {
+    private Alliance alliance;
     public ChessPiece(String imageName) {
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pieces/" + imageName)));
         setFitHeight(ChessBoard.TILE_SIZE);
         setFitWidth(ChessBoard.TILE_SIZE);
         setImage(image);
+        if (imageName.contains("white")) setAlliance(Alliance.WHITE);
+        else setAlliance(Alliance.BLACK);
+    }
+    public void setAlliance(Alliance alliance) {
+        this.alliance = alliance;
+    }
+
+    public Alliance getAlliance() {
+        return alliance;
     }
 }
