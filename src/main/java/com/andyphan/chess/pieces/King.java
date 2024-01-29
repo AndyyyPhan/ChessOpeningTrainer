9108205d4bb72bee256a869128204183d7449528
Andy Phan <AndyyyPhan@gmail.com>
2024-01-29 10:37:35 -0500

    Added hasMove and its getters and setters to implement castling

--- src/main/java/com/andyphan/chess/pieces/King.java
+++ src/main/java/com/andyphan/chess/pieces/King.java
@@ -3,7 +3,16 @@ package com.andyphan.chess.pieces;
 import com.andyphan.chess.ChessPiece;
 
 public class King extends ChessPiece {
+    private boolean hasMoved = false;
     public King(String imagePath) {
         super(imagePath);
+    }
+
+    public void setHasMoved(boolean hasMoved) {
+        this.hasMoved = hasMoved;
+    }
+
+    public boolean getHasMoved() {
+        return hasMoved;
     }
 }
