4d39af38bf9908e436129cf30602d3c2dfd03e8c
Andy Phan <AndyyyPhan@gmail.com>
2024-05-17 19:41:09 -0400

    Override initializeButtons()

--- src/main/java/com/andyphan/chessopeningtrainer/CreateChessOpening.java
+++ src/main/java/com/andyphan/chessopeningtrainer/CreateChessOpening.java
@@ -1,9 +1,21 @@
 package com.andyphan.chessopeningtrainer;
 
 import com.andyphan.chess.ChessScene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class CreateChessOpening extends ChessScene {
     public CreateChessOpening(ChessOpening chessOpening) {
         super(chessOpening);
+    }
+
+    @Override
+    protected void initializeButtons() {
+        super.initializeButtons();
+        layout.getChildren().removeAll(flipBoardButton, showAllMovesButton);
+        showAllMovesButton.setDisable(true);
+        Button save = new Button("Save");
+        Button exit = new Button("Exit");
+        layout.getChildren().addAll(new HBox(5, flipBoardButton, showAllMovesButton), new HBox(5, save, exit));
     }
 }
