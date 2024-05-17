d733938d725907d1cc70868196f3411fbfef716b
Andy Phan <AndyyyPhan@gmail.com>
2024-05-16 22:21:13 -0400

    Added CreateChessOpening.java, which will be the scene that handles creating openings

---
+++ src/main/java/com/andyphan/chessopeningtrainer/CreateChessOpening.java
@@ -0,0 +1,9 @@
+package com.andyphan.chessopeningtrainer;
+
+import com.andyphan.chess.ChessScene;
+
+public class CreateChessOpening extends ChessScene {
+    public CreateChessOpening(ChessOpening chessOpening) {
+        super(chessOpening);
+    }
+}
