e2ac2e749a0b52f1ccf9d29b327f6e34a713e25b
Andy Phan <AndyyyPhan@gmail.com>
2024-05-29 16:00:50 -0400

    Added algebraic notation for knight moves

--- src/main/java/com/andyphan/chessopeningtrainer/CreateChessOpening.java
+++ src/main/java/com/andyphan/chessopeningtrainer/CreateChessOpening.java
@@ -2,15 +2,20 @@ package com.andyphan.chessopeningtrainer;
 
 import com.andyphan.chess.Alliance;
 import com.andyphan.chess.ChessScene;
+import com.andyphan.chess.pieces.King;
+import com.andyphan.chess.pieces.Knight;
 import com.andyphan.chess.pieces.Pawn;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+
+import static com.andyphan.chess.ChessBoard.BOARD_SIZE;
 
 public class CreateChessOpening extends ChessScene {
     private String moves;
     private StringBuilder stringBuilder = new StringBuilder();
     private StringBuilder firstHalf = new StringBuilder();
     private int moveCounter = 1;
+
     public CreateChessOpening(ChessOpening chessOpening) {
         super(chessOpening);
     }
@@ -31,28 +36,89 @@ public class CreateChessOpening extends ChessScene {
 
     @Override
     protected void selectPiece(int col, int row) {
-        super.selectPiece(col, row);
+        selectedPiece = chessGrid[row][col].getChessPiece();
+        boolean isValidSelection;
+        if (selectedPiece != null && playerTurn.getCurrentTurn() == selectedPiece.getAlliance()) {
+            selectedTile.setRowAndCol(row, col);
+            selectedTile.setChessPiece(chessGrid[row][col].getChessPiece());
+            selectedRow = row;
+            selectedCol = col;
+            isValidSelection = true;
+        }
+        else {
+            isValidSelection = false;
+        }
+
         if (isValidSelection) {
             if (playerTurn.getCurrentTurn() == Alliance.WHITE) {
                 stringBuilder.append(moveCounter).append(". ");
                 moveCounter++;
             }
-            if (selectedPiece.getClass() == Pawn.class) {
-                firstHalf.append(selectedTile.getTileName());
+            if (selectedPiece.getClass() == Knight.class) {
+                firstHalf.append("N"); // REMOVE: Update tile's getTileName() method so this is done automatically.
             }
+            firstHalf.append(selectedTile.getTileName());
         }
     }
 
     @Override
     protected void movePiece(int targetCol, int targetRow) {
-        super.movePiece(targetCol, targetRow);
+        selectedPiece = selectedTile.getChessPiece();
+        targetTile = chessGrid[targetRow][targetCol];
+        targetPiece = targetTile.getChessPiece();
+        boolean isValidMove = false;
+        boolean isOtherKnightMove = false;
+        if (selectedPiece != targetPiece && selectedPiece.isValidMove(targetTile)) {
+            if (selectedPiece.getClass() == Knight.class) {
+                for (int row = 0; row < BOARD_SIZE; row++) {
+                    for (int col=0; col < BOARD_SIZE; col++) {
+                        if ((selectedTile.getRow() != row && selectedTile.getCol() != col) &&
+                                chessBoard.getChessGridTileByName(row, col).getChessPiece() != null &&
+                                chessBoard.getChessGridTileByName(row, col).getChessPiece().getAlliance() == playerTurn.getCurrentTurn() &&
+                                chessBoard.getChessGridTileByName(row, col).getChessPiece().getClass() == Knight.class) {
+                            if (chessBoard.getChessGridTileByName(row, col).getChessPiece().isValidMove(targetTile)) {
+                                isOtherKnightMove = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (selectedPiece.getClass() == King.class && ((King) selectedPiece).isCastling()) {
+                handleCastling(targetRow, selectedPiece);
+            }
+            else {
+                chessBoard.getChildren().remove(selectedPiece);
+                chessGrid[selectedRow][selectedCol].setChessPiece(null);
+                selectedTile.resetTile();
+                if (targetPiece != null) chessBoard.getChildren().remove(targetPiece);
+                targetTile.setChessPiece(selectedPiece);
+                chessBoard.add(selectedPiece, targetCol, targetRow);
+                selectedPiece.setTile(targetTile);
+                playerTurn.setNextTurn();
+            }
+            isValidMove = true;
+        }
+        selectedTile.setRowAndCol(-1, -1);
+
         if (isValidMove) {
             if (selectedPiece.getClass() == Pawn.class) {
-                if (targetPiece != null) {
-                    stringBuilder.append(firstHalf.toString().charAt(0)).append("x").append(targetTile.getTileName()).append(" ");
+                if (targetPiece == null) {
+                    stringBuilder.append(targetTile.getTileName()).append(" ");
                 }
                 else {
-                    stringBuilder.append(targetTile.getTileName()).append(" ");
+                    stringBuilder.append(firstHalf.toString().charAt(0)).append("x").append(targetTile.getTileName()).append(" ");
+                }
+            }
+            else if (selectedPiece.getClass() == Knight.class) {
+                if (targetPiece == null) {
+                    {
+                        if (isOtherKnightMove) stringBuilder.append(firstHalf.substring(0, 2)).append(targetTile.getTileName()).append(" ");
+                        else stringBuilder.append(firstHalf.toString().charAt(0)).append(targetTile.getTileName()).append(" ");
+                    }
+                }
+                else {
+                    if (isOtherKnightMove) stringBuilder.append(firstHalf.substring(0, 2)).append("x").append(targetTile.getTileName()).append(" ");
+                    else stringBuilder.append(firstHalf.toString().charAt(0)).append("x").append(targetTile.getTileName()).append(" ");
                 }
             }
             moves = stringBuilder.toString();
