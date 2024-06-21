3798e0e52fd73f710a09dcb54690c30463566545
Andy Phan <AndyyyPhan@gmail.com>
2024-06-21 14:04:10 -0400

    Implemented Singleton data pattern. Also, made better session/transaction management. Implemented persistChessOpening() and addChessOpeningToPractice()

--- src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
+++ src/main/java/com/andyphan/chessopeningtrainer/DatabaseManager.java
@@ -1,40 +1,55 @@
 package com.andyphan.chessopeningtrainer;
 
 import jakarta.persistence.PersistenceException;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.*;
-import org.hibernate.query.Query;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 public class DatabaseManager {
-    private Session session;
+    private static DatabaseManager instance;
+    private final SessionFactory sessionFactory;
 
     public DatabaseManager() {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
-        this.session = HibernateUtil.getSessionFactory().openSession();
+        this.sessionFactory = HibernateUtil.getSessionFactory();
     }
 
-    public void close() {
-        session.close();
+    public static synchronized DatabaseManager getInstance() {
+        if (instance == null) instance = new DatabaseManager();
+        return instance;
     }
 
     public boolean isUserExisting(String username) {
-        User existingUser = session.createQuery("FROM User WHERE username = :username", User.class)
-                .setParameter("username", username)
-                .uniqueResult();
-        return existingUser != null;
+        try (Session session = sessionFactory.openSession()){
+            User existingUser = session.createQuery("FROM User WHERE username = :username", User.class)
+                    .setParameter("username", username)
+                    .uniqueResult();
+            return existingUser != null;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean isValidUser(String username, String password) {
         if (!isUserExisting(username)) return false;
 
-        User existingUser = session.createQuery("FROM User WHERE username = :username", User.class)
-                .setParameter("username", username)
-                .uniqueResult();
-        return existingUser.getPassword().equals(password);
+        try (Session session = sessionFactory.openSession()) {
+            User existingUser = session.createQuery("FROM User WHERE username = :username", User.class)
+                    .setParameter("username", username)
+                    .uniqueResult();
+            return existingUser.getPassword().equals(password);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean validPasswordLength(String password) {
@@ -42,78 +57,187 @@ public class DatabaseManager {
     }
 
     public void addUser(String username, String password) {
-        session.beginTransaction();
-        try {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
             User user = new User();
             user.setUsername(username);
             user.setPassword(password);
             session.persist(user);
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (PersistenceException e) {
-            if (session.getTransaction() != null) session.getTransaction().rollback();
+            if (transaction != null) transaction.rollback();
         }
     }
 
     public User getUser(String username) {
-        return session.createQuery("FROM User WHERE username =:username", User.class)
-                .setParameter("username", username)
-                .uniqueResult();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User WHERE username =:username", User.class)
+                    .setParameter("username", username)
+                    .uniqueResult();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public List<ChessOpening> getOpeningsInPractice() {
-        List<ChessOpening> chessOpenings = Collections.emptyList();
-        session.beginTransaction();
-        try {
-            Query<ChessOpening> query = session.createQuery("FROM ChessOpening", ChessOpening.class);
-            chessOpenings = query.getResultList();
-            session.getTransaction().commit();
-        }
-        catch (Exception e) {
-            if (session.getTransaction() != null) session.getTransaction().rollback();
-        }
-        return chessOpenings;
-    }
-
-    public void addChessOpening(ChessOpening chessOpening) throws IOException {
-        if (chessOpening.getFen().isBlank()) chessOpening.setFen(ChessOpeningsParser.getNextCreatedOpeningFen());
-        User currentUser = UserManager.getCurrentUser();
-        chessOpening.setUser(currentUser);
-        session.beginTransaction();
-        try {
-            session.persist(chessOpening);
-            session.getTransaction().commit();
-        } catch (PersistenceException e) {
-            if (session.getTransaction() != null) session.getTransaction().rollback();
-        }
-    }
-
-    public boolean isChessOpeningExisting(ChessOpening chessOpening) {
-        ChessOpening existingChessOpening = session.createQuery("FROM ChessOpening WHERE fen = :fen", ChessOpening.class)
-                .setParameter("fen", chessOpening.getFen())
-                .uniqueResult();
-        return existingChessOpening != null;
-    }
-
-    public void deleteChessOpening(ChessOpening chessOpening) {
-        session.beginTransaction();
-        try {
-            session.createQuery("DELETE FROM ChessOpening WHERE fen = :fen AND name = :name AND moves = :moves")
-                    .setParameter("fen", chessOpening.getFen())
-                    .setParameter("name", chessOpening.getName())
-                    .setParameter("moves", chessOpening.getMoves())
-                    .executeUpdate();
-            session.getTransaction().commit();
+        List<ChessOpening> practiceOpenings = Collections.emptyList();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User currentUser = session.get(User.class, UserManager.getCurrentUser().getId());
+            System.out.println(currentUser.getUsername());
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<ActiveChessOpening> criteriaQuery = criteriaBuilder.createQuery(ActiveChessOpening.class);
+            Root<ActiveChessOpening> root = criteriaQuery.from(ActiveChessOpening.class);
+            criteriaQuery.where(criteriaBuilder.equal(root.get("user"), currentUser));
+            TypedQuery<ActiveChessOpening> typedQuery = session.createQuery(criteriaQuery);
+            List<ActiveChessOpening> practiceOpeningsList = typedQuery.getResultList();
+            practiceOpenings = practiceOpeningsList.stream()
+                    .map(ActiveChessOpening::getChessOpening)
+                    .collect(Collectors.toList());
+            transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
-            if (session.getTransaction() != null) session.getTransaction().rollback();
+            if (transaction != null) transaction.rollback();
+        }
+        return practiceOpenings;
+    }
+
+    public void persistChessOpening(ChessOpening chessOpening) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            session.persist(chessOpening);
+            transaction.commit();
+        } catch (PersistenceException e) {
+            if (transaction != null) transaction.rollback();
         }
     }
 
-    public ChessOpening getChessOpening(String fen, String name, String moves) {
-        return session.createQuery("FROM ChessOpening WHERE fen = :fen AND name = :name AND moves = :moves", ChessOpening.class)
-                .setParameter("fen", fen)
-                .setParameter("name", name)
-                .setParameter("moves", moves)
-                .uniqueResult();
+    public void addChessOpeningToPractice(ChessOpening chessOpening) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()){
+            transaction = session.beginTransaction();
+            User currentUser = session.get(User.class, UserManager.getCurrentUser().getId());
+            ChessOpening persistentChessOpening = session.get(ChessOpening.class, chessOpening.getId());
+
+            if (!isChessOpeningInPractice(persistentChessOpening)) {
+                ActiveChessOpening activeChessOpening = new ActiveChessOpening();
+                activeChessOpening.setUser(currentUser);
+                activeChessOpening.setChessOpening(persistentChessOpening);
+                session.persist(activeChessOpening);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) transaction.rollback();
+        }
+    }
+
+    private boolean isChessOpeningInPractice(ChessOpening chessOpening) {
+        boolean isInPractice = false;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            User currentUser = session.get(User.class, UserManager.getCurrentUser().getId());
+
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<ActiveChessOpening> criteriaQuery = criteriaBuilder.createQuery(ActiveChessOpening.class);
+            Root<ActiveChessOpening> root = criteriaQuery.from(ActiveChessOpening.class);
+
+            criteriaQuery.select(root)
+                    .where(
+                            criteriaBuilder.and(
+                                    criteriaBuilder.equal(root.get("user"), currentUser),
+                                    criteriaBuilder.equal(root.get("chessOpening"), chessOpening)
+                            )
+                    );
+            TypedQuery<ActiveChessOpening> query = session.createQuery(criteriaQuery);
+            List<ActiveChessOpening> results = query.getResultList();
+            isInPractice = !results.isEmpty();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) transaction.rollback();
+        }
+        return isInPractice;
+    }
+
+//    public void addChessOpening(ChessOpening chessOpening) throws IOException {
+//        Transaction transaction = null;
+//        try (Session session = sessionFactory.openSession()) {
+//            transaction = session.beginTransaction();
+//            if (chessOpening.getFen().isBlank()) chessOpening.setFen(ChessOpeningsParser.getNextCreatedOpeningFen());
+//            User currentUser = UserManager.getCurrentUser();
+//            chessOpening.setUser(currentUser);
+//            session.persist(chessOpening);
+//            transaction.commit();
+//        } catch (PersistenceException e) {
+//            e.printStackTrace();
+//            if (transaction != null) transaction.rollback();
+//        }
+//    }
+//
+//    public boolean isChessOpeningExisting(ChessOpening chessOpening) {
+//        ChessOpening existingChessOpening = session.createQuery("FROM ChessOpening WHERE fen = :fen", ChessOpening.class)
+//                .setParameter("fen", chessOpening.getFen())
+//                .uniqueResult();
+//        return existingChessOpening != null;
+//    }
+//
+//    public void deleteChessOpening(ChessOpening chessOpening) {
+//        session.beginTransaction();
+//        try {
+//            session.createQuery("DELETE FROM ChessOpening WHERE fen = :fen AND name = :name AND moves = :moves")
+//                    .setParameter("fen", chessOpening.getFen())
+//                    .setParameter("name", chessOpening.getName())
+//                    .setParameter("moves", chessOpening.getMoves())
+//                    .executeUpdate();
+//            session.getTransaction().commit();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            if (session.getTransaction() != null) session.getTransaction().rollback();
+//        }
+//    }
+//
+//    public ChessOpening getChessOpening(String fen, String name, String moves) {
+//        return session.createQuery("FROM ChessOpening WHERE fen = :fen AND name = :name AND moves = :moves", ChessOpening.class)
+//                .setParameter("fen", fen)
+//                .setParameter("name", name)
+//                .setParameter("moves", moves)
+//                .uniqueResult();
+//    }
+//
+    public boolean loginUser(String username, String password) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            criteriaQuery.where(
+                    criteriaBuilder.and(
+                            criteriaBuilder.equal(root.get("username"), username),
+                            criteriaBuilder.equal(root.get("password"), password)
+                    )
+            );
+            TypedQuery<User> query = session.createQuery(criteriaQuery);
+            User user = query.getSingleResult();
+            if (user != null) {
+                UserManager.setCurrentUser(user);
+                System.out.println("User logged in: " + user.getUsername());
+                session.getTransaction().commit();
+                return true;
+            } else {
+                System.out.println("Invalid login credentials.");
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) transaction.rollback();
+            return false;
+        }
     }
 }
