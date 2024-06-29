a7ff9972c8db076f290a0cab81ded35de212ea5a
Andy Phan <AndyyyPhan@gmail.com>
2024-06-29 14:16:27 -0400

    Added UserChessOpening to handle user-specific chess openings (chess openings that users create).

---
+++ src/main/java/com/andyphan/chessopeningtrainer/UserChessOpening.java
@@ -0,0 +1,43 @@
+package com.andyphan.chessopeningtrainer;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserChessOpenings")
+public class UserChessOpening {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chess_opening_id", nullable = false)
+    private ChessOpening chessOpening;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ChessOpening getChessOpening() {
+        return chessOpening;
+    }
+
+    public void setChessOpening(ChessOpening chessOpening) {
+        this.chessOpening = chessOpening;
+    }
+}
