ca34f8508fca76e4b89f954e6f3c7b4eca4b91f3
Andy Phan <AndyyyPhan@gmail.com>
2024-07-07 12:00:48 -0400

    Implemented isCheck for Pawn

--- src/main/java/com/andyphan/chess/pieces/Pawn.java
+++ src/main/java/com/andyphan/chess/pieces/Pawn.java
@@ -33,4 +33,23 @@ public class Pawn extends ChessPiece {
         }
         return false;
     }
+
+    @Override
+    public boolean isCheck() {
+        if (getAlliance() == Alliance.WHITE) {
+            Tile possibleLeftCheckTile = chessBoard.getChessGridTileByName(getRow() - 1, getCol() - 1);
+            Tile possibleRightCheckTile = chessBoard.getChessGridTileByName(getRow() - 1, getCol() + 1);
+            if (possibleLeftCheckTile.getChessPiece() == null && possibleRightCheckTile.getChessPiece() == null) return false;
+            return ((possibleLeftCheckTile.getChessPiece() != null && possibleLeftCheckTile.getChessPiece().getClass() == King.class && possibleLeftCheckTile.getChessPiece().getAlliance() == Alliance.BLACK)
+                    || (possibleRightCheckTile.getChessPiece() != null && possibleRightCheckTile.getChessPiece().getClass() == King.class && possibleRightCheckTile.getChessPiece().getAlliance() == Alliance.BLACK));
+        }
+        else if (getAlliance() == Alliance.BLACK) {
+            Tile possibleLeftCheckTile = chessBoard.getChessGridTileByName(getRow() + 1, getCol() - 1);
+            Tile possibleRightCheckTile = chessBoard.getChessGridTileByName(getRow() + 1, getCol() + 1);
+            if (possibleLeftCheckTile.getChessPiece() == null && possibleRightCheckTile.getChessPiece() == null) return false;
+            return ((possibleLeftCheckTile.getChessPiece() != null && possibleLeftCheckTile.getChessPiece().getClass() == King.class && possibleLeftCheckTile.getChessPiece().getAlliance() == Alliance.WHITE)
+                    || (possibleRightCheckTile.getChessPiece() != null && possibleRightCheckTile.getChessPiece().getClass() == King.class && possibleRightCheckTile.getChessPiece().getAlliance() == Alliance.WHITE));
+        }
+        return false;
+    }
 }
