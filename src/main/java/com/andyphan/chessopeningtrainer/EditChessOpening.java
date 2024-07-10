ea612bf60c9e501a0efe451c191b221b4c028b0f
Andy Phan <AndyyyPhan@gmail.com>
2024-07-10 11:58:38 -0400

    Added EditChessOpening scene

---
+++ src/main/java/com/andyphan/chessopeningtrainer/EditChessOpening.java
@@ -0,0 +1,9 @@
+package com.andyphan.chessopeningtrainer;
+
+import com.andyphan.database.ChessOpening;
+
+public class EditChessOpening extends CreateChessOpening {
+    public EditChessOpening(ChessOpening chessOpening) {
+        super(chessOpening);
+    }
+}
